b9efe7a8a64b41455217109c54c3ce1362f87b7e
Zed <35542555+Zedstar16@users.noreply.github.com>
2024-10-22 22:17:24 +0100

    Add implementation of register player ot to Sqlite3Provider.php

--- src/Zedstar16/OnlineTime/database/Sqlite3Provider.php
+++ src/Zedstar16/OnlineTime/database/Sqlite3Provider.php
@@ -9,17 +9,33 @@ class Sqlite3Provider extends ProviderInterface
     public function initTables(): void {
         DatabaseThreadHandler::add("
             CREATE TABLE IF NOT EXISTS XuidRelation(
-                xuid TEXT NOT NULL UNIQUE PRIMARY KEY,
-                username TEXT
+                xuid VARCHAR(16) NOT NULL UNIQUE PRIMARY KEY,
+                username VARCHAR(16)
             );                                      
         ");
         DatabaseThreadHandler::add("
             CREATE TABLE IF NOT EXISTS PlayerSessions(
-                xuid TEXT NOT NULL,
+                xuid VARCHAR(16) NOT NULL,
                 session_start_time INT,
                 session_duration INT
             );                         
         ");
+    }
+
+    /**
+     * Registers player if they do not exist in db and updates username if changed
+     */
+    public function register(string $xuid, string $username): void {
+        $username = strtolower($username);
+        DatabaseThreadHandler::add("SELECT username from XuidRelation where xuid = '$xuid'", function ($result) use ($xuid, $username) {
+            if (($result["username"] ?? null) === null) {
+                DatabaseThreadHandler::add("INSERT OR IGNORE INTO XuidRelation VALUES('$xuid', '$username')");
+                return;
+            }
+            if ($result["username"] !== $username) {
+                DatabaseThreadHandler::add("UPDATE XuidRelation SET username = '$username' where xuid = '$xuid'");
+            }
+        });
     }
 
 }
